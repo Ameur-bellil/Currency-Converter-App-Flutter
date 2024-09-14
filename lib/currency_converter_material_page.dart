47fdb9b861abfaa719c37e63d4ca76d1a16ea3d0
Amer <ameur.bellil@eniso.u-sousse.tn>
2024-09-14 15:00:08 +0100

    small changes

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,11 +1,14 @@
-
 import 'package:flutter/material.dart';
+
+
+
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print('rebuild ');
     double result=0;
     final TextEditingController textEditingController = TextEditingController() ;
     final border = OutlineInputBorder(
