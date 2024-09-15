a787091ae771f2861ad1b360cf61a5cb80ab1af7
Amer <ameur.bellil@eniso.u-sousse.tn>
2024-09-15 22:33:46 +0100

    "change StatefulWidget"

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage ({super.key});
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
 
+class _CurrencyConverterMaterialPageState extends State<CurrencyConverterMaterialPage>{
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
-  const CurrencyConverterMaterialPage({super.key});
+  double result=0;
+  final TextEditingController textEditingController = TextEditingController() ;
 
   @override
   Widget build(BuildContext context) {
-    print('rebuild ');
-    double result=0;
-    final TextEditingController textEditingController = TextEditingController() ;
     final border = OutlineInputBorder(
       borderSide: const  BorderSide(
         color: Colors.black,
@@ -35,70 +39,72 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         ),
       ),
       body: Center(
-            child: Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               Text(
-                result.toString(),
-                style: const TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 255, 255)
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '${result}  TND',
+              style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)
               ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: textEditingController,
-                  style:const TextStyle(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: textEditingController,
+                style:const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount in USD',
+                  hintStyle: const TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: InputDecoration(
-                    hintText: 'Please enter the amount in USD',
-                    hintStyle: const TextStyle(
-                      color: Colors.black,
-                    ),
-                    prefixIcon: const Icon(
-                        Icons.monetization_on_outlined
-                    ),
-                    prefixIconColor: Colors.black,
-                    fillColor: Colors.white,
-                    filled: true,
-                    focusedBorder: border,
-                    enabledBorder: border,
-                    ),
-                  keyboardType: const TextInputType.numberWithOptions(
+                  prefixIcon: const Icon(
+                      Icons.monetization_on_outlined
+                  ),
+                  prefixIconColor: Colors.black,
+                  fillColor: Colors.white,
+                  filled: true,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
                     decimal: true
-                  ),
-                  ),
+                ),
               ),
-              /// buttons :  1. raised ElevatedButton 2. appears like a text TextButton
-               Padding(
-                 padding: const EdgeInsets.all(10),
-                 child: ElevatedButton(
-                   onPressed: () {
-                     result = double.parse(textEditingController.text)*3.055;
-                     print(double.parse(textEditingController.text)*3.055);
-                   },
-                   style:  ElevatedButton.styleFrom(
-                     backgroundColor:  Colors.black,
-                     foregroundColor:
-                         Colors.white,
-                     minimumSize: const Size(
-                         double.infinity,50
-                     ),
-                     elevation:15,
-                     shape: RoundedRectangleBorder(
-                         borderRadius: BorderRadius.circular(10)
-                     ),
-
-                   ),
-                   child: const Text('Convert'),
-                 ),
-               ),
-             ],   // Children
             ),
-          ), 
-        );
+            /// buttons :  1. raised ElevatedButton 2. appears like a text TextButton
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text)*3.055;
+
+                  });  
+                },
+                style:  ElevatedButton.styleFrom(
+                  backgroundColor:  Colors.black,
+                  foregroundColor:
+                  Colors.white,
+                  minimumSize: const Size(
+                      double.infinity,50
+                  ),
+                  elevation:15,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                ),
+                child: const Text('Convert'),
+              ),
+            ),
+          ],   // Children
+        ),
+      ),
+    );
   }
 }
